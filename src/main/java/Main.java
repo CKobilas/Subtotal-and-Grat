de2ce0058986d36360611e2aa9359b85e533da36
CKobilas <156360780+CKobilas@users.noreply.github.com>
2024-02-20 16:28:11 +0000

    Added Pseudocode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,28 @@ public class Main {
 
   public static void main(String[] args) 
   {
-
+    // PRINT "Please enter a subtotal and gratuity rate:  “
+    
     System.out.println("Please enter a subtotal and gratuity rate:   ");
-
+    
+    // CREATE Scanner object “money” for user input
+    
     Scanner money = new Scanner(System.in);
-
+    
+    // READ subtotal from user input
+    // READ gratuity rate from user input
+    
     double subtotal = money.nextDouble();
     double gratuity = money.nextDouble();
-
+    
+    // CALCULATE the total amount 
+    
     double grat = (gratuity / 100) * subtotal;
 
     double total = subtotal + grat;
 
+    // PRINT the total amount "The gratuity is $" + grat + " and the total is $" + total
+    
     System.out.println("The gratuity is $" + grat + " and the total is $" + total);
 
   }
